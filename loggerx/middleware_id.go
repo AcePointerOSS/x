f78f24647c82a999cc7d5f7c5eda26f15a02184a
Jing Kai Tan <contact@jingk.ai>
2021-02-10 00:57:02 +0800

    feat: prepare logger

---
+++ loggerx/middleware_id.go
@@ -0,0 +1,18 @@
+package loggerx
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gofrs/uuid"
+)
+
+func UseRequestId() gin.HandlerFunc {
+	return func(c *gin.Context){
+		// dont preface with x as per RFC 6648 (June 2012)
+		reqId := c.GetHeader("request-id")
+		if reqId == "" {
+			reqId = uuid.New().String()
+		}
+		c.Header("request-id", reqId)
+		c.Next()
+	}
+}
