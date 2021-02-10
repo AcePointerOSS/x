f002f6d73ae8a61e3087e943ea43bf845701fbcf
JingKai Tan <contact@jingk.ai>
2021-02-10 17:09:17 +0800

    feat: adding loggerx

--- loggerx/middleware_id.go
+++ loggerx/middleware_id.go
@@ -5,12 +5,16 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+func GetReqId(c *gin.Context) string {
+	return c.GetHeader("request-id")
+}
+
 func UseRequestId() gin.HandlerFunc {
-	return func(c *gin.Context){
+	return func(c *gin.Context) {
 		// dont preface with x as per RFC 6648 (June 2012)
 		reqId := c.GetHeader("request-id")
 		if reqId == "" {
-			reqId = uuid.New().String()
+			reqId = uuid.Must(uuid.NewV4()).String()
 		}
 		c.Header("request-id", reqId)
 		c.Next()
